26569e6571dcb3e2b9a078091258d8c65167a086
Dmitriy <ponomarev_d.a@mail.ru>
2016-02-09 23:20:21 +0300

    remove failed push

--- src/main/java/ru/analteam/gtracks/model/converter/security/SecRole2GrantedAuthorityListConverter.java
+++
@@ -1,7 +0,0 @@
-package ru.analteam.gtracks.model.converter.security;
-
-/**
- * Created by dima-pc on 07.02.2016.
- */
-public class SecRole2GrantedAuthorityConverter implements  {
-}
--- src/main/java/ru/analteam/gtracks/model/dto/quiz/QuizTheme.java
+++
@@ -1,7 +0,0 @@
-package ru.analteam.gtracks.model.dto.quiz;
-
-/**
- * Created by dima-pc on 06.12.2015.
- */
-public class QuizTheme {
-}
