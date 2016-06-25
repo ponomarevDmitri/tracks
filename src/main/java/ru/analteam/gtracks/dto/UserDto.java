3e5ab33fdebd8b6663b6c9082fbb23708cf55a60
Dmitriy <ponomarev_d.a@mail.ru>
2016-06-26 00:26:03 +0300

    Test for RouteDto2RouteConverter

--- src/main/java/ru/analteam/gtracks/dto/UserDto.java
+++ src/main/java/ru/analteam/gtracks/dto/UserDto.java
@@ -7,6 +7,19 @@ public class UserDto {
     private String username;
     private String password;
 
+
+    public UserDto() {
+    }
+
+    public UserDto(String username) {
+        this.username = username;
+    }
+
+    public UserDto(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
     public String getUsername() {
         return username;
     }
---
+++ src/test/java/ru/analteam/gtracks/model/converter/security/RouteDto2RouteConverterTest.java
@@ -0,0 +1,72 @@
+package ru.analteam.gtracks.model.converter.security;
+
+import org.junit.Assert;
+import org.junit.Test;
+import ru.analteam.gtracks.dto.RouteDto;
+import ru.analteam.gtracks.dto.RoutePointDto;
+import ru.analteam.gtracks.dto.UserDto;
+import ru.analteam.gtracks.model.route.Route;
+import ru.analteam.gtracks.model.route.RoutePoint;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Test for {@link RouteDto2RouteConverter}
+ * Created by dima-pc on 25.06.2016.
+ */
+public class RouteDto2RouteConverterTest {
+    @Test
+    public void testConvert() throws Exception {
+        RouteDto2RouteConverter converter = new RouteDto2RouteConverter();
+
+        RouteDto source = new RouteDto();
+
+        source.setDescription("description");
+        source.setShortDescription("short description");
+        source.setName("name");
+        source.setUser(new UserDto("username"));
+
+        ArrayList<RoutePointDto> routePointDtos = new ArrayList<RoutePointDto>();
+        RoutePointDto routePointDto = new RoutePointDto();
+        routePointDto.setShortDescription("first point");
+        routePointDto.setDescription("first point description");
+        routePointDto.setLatitude(11.111111d);
+        routePointDto.setLongitude(111.222222d);
+        routePointDtos.add(routePointDto);
+
+        routePointDto = new RoutePointDto();
+        routePointDto.setShortDescription("second point");
+        routePointDto.setDescription("second point description");
+        routePointDto.setLatitude(22.333333d);
+        routePointDto.setLongitude(122.333333d);
+        routePointDtos.add(routePointDto);
+
+        source.setRoute(routePointDtos);
+
+
+        Route converted = converter.convert(source);
+
+
+        Assert.assertNotNull(converted);
+        Assert.assertEquals("description", converted.getDescription());
+        Assert.assertEquals("short description", converted.getShortDescription());
+        Assert.assertEquals("name", converted.getName());
+
+        List<RoutePoint> convertedRoutePointList = converted.getRoutePoints();
+        Assert.assertNotNull(convertedRoutePointList);
+        Assert.assertEquals(2, convertedRoutePointList.size());
+
+        RoutePoint convertedRoutePoint = convertedRoutePointList.get(0);
+        Assert.assertEquals("first point", convertedRoutePoint.getShortDescription());
+        Assert.assertEquals("first point description", convertedRoutePoint.getDescription());
+        Assert.assertEquals(Double.valueOf(11.111111d), convertedRoutePoint.getGeoCoordinate().getLatitude());
+        Assert.assertEquals(Double.valueOf(111.222222d), convertedRoutePoint.getGeoCoordinate().getLongitude());
+
+        convertedRoutePoint = convertedRoutePointList.get(1);
+        Assert.assertEquals("second point", convertedRoutePoint.getShortDescription());
+        Assert.assertEquals("second point description", convertedRoutePoint.getDescription());
+        Assert.assertEquals(Double.valueOf(22.333333d), convertedRoutePoint.getGeoCoordinate().getLatitude());
+        Assert.assertEquals(Double.valueOf(122.333333d), convertedRoutePoint.getGeoCoordinate().getLongitude());
+    }
+}
